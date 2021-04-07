7ffc8b024057e53eaf346b50066590a7fd133187
thucngyyen <thucnguyen1952000@gmail.com>
2021-04-07 00:14:45 -0700

    pep8 checked

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'coffee-shop-thucngyyen.us', // the auth0 domain prefix
+    audience: 'coffee_shop_menu', // the audience set for the auth0 app
+    clientId: 'ADVnrhFnHDs2IfElVaxFLzIPwH31K0tn', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
